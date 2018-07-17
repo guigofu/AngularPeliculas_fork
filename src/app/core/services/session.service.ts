b22326c80eb4e3b9e401b67c1cbf76f1a80f9dff
Guillermo Gomez <gui_go91@hotmail.com>
2018-07-16 20:03:54 -0500

    example of sessionService with sessionStorage

--- src/app/core/services/session.service.ts
+++ src/app/core/services/session.service.ts
@@ -5,7 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class SessionService {
 
-  logged = false;
+  get logged(): boolean{
+    return sessionStorage.getItem('logged') === 'true';
+  }
+
+  set logged(value: boolean){
+    sessionStorage.setItem('logged', '' + value);
+  }
+
+  // para guardar objetos (debe ser como string) en sessionStorage
+  // saveObject(obj: any){
+  //   sessionStorage.setItem('key', JSON.stringify(obj));
+  // }
 
   constructor() { }
 }
